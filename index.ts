46994dbb96e599f047e1573dd9564dfb9aed3746
AafiaUzair <aafiauzair1@gmail.com>
2024-05-03 14:07:01 +0500

    This is a todo app using TypeScript

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;
+while(condition){
+    let todoQuestions = await inquirer.prompt([{
+        name: "firstQuestion",
+        message: "What you want to add in your todo list?",
+        type: "input"
+    },
+{
+    name: "secondQuestion",
+    message: "Do you want to add more?",
+    type: "confirm",
+    default: "true"
+}]);
+todos.push(todoQuestions.firstQuestion);
+console.log(todos);
+condition = todoQuestions.secondQuestion;
+}
